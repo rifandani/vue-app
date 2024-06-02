cb9965c4a6c92024f76e9c8cf9a135cca3cb6c2b
rifandani <ipandani2505@gmail.com>
2024-06-02 13:57:31 +0700

    feat(ui): carousel

---
+++ src/modules/shared/components/ui/carousel/CarouselContent.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { twMerge } from 'tailwind-merge'
+import { useCarousel } from './useCarousel'
+import type { WithClassAsProps } from './interface'
+
+defineOptions({
+  inheritAttrs: false,
+})
+
+const props = defineProps<WithClassAsProps>()
+
+const { carouselRef, orientation } = useCarousel()
+</script>
+
+<template>
+  <div ref="carouselRef" class="overflow-hidden">
+    <div
+      :class="
+        twMerge(
+          'flex',
+          orientation === 'horizontal' ? '-ml-4' : '-mt-4 flex-col',
+          props.class,
+        )"
+      v-bind="$attrs"
+    >
+      <slot />
+    </div>
+  </div>
+</template>
